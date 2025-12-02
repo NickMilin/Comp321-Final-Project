727af27b057aff4dd85de5d009a2ff97b74b9226
KiwiSorbet <galaxygum51@gmail.com>
2025-12-02 17:14:25 -0500

    Removed unused import

--- submissions/time_limit_exceeded/time_limit_exceeded.py
+++ submissions/time_limit_exceeded/time_limit_exceeded.py
@@ -1,5 +1,3 @@
-import time
-
 def solve():
     # start_time = time.perf_counter()
 
@@ -7,13 +5,13 @@ def solve():
     N, M = list(map(int, input().split()))
 
     # --- PHASE 1: BEAUTIFICATION UPDATES ---
-    '''Create a list of scenic values and update list directly'''
+    """Create a list of scenic values and update list directly"""
     scenic_values = [0] * (N + 1)
 
     for j in range(M):
         L, R, V = list(map(int, input().split()))
         # Mark increase and decrease of value segments
-        for i in range(L, R+1):
+        for i in range(L, R + 1):
             scenic_values[i] += V
 
     # --- PHASE 2: TRAFFIC SCENARIOS ---
@@ -21,24 +19,24 @@ def solve():
 
     for _ in range(Q):
         accidents = list(map(int, input().split()))
-        
+
         # Sort accidents
         accidents.sort()
-        
+
         # Add "Virtual Barriers" at 0 (start) and N+1 (end)
         barriers = [0] + accidents + [N + 1]
-        
-        max_trip_value = -float('inf')
+
+        max_trip_value = -float("inf")
         driveable = False
 
         # Check every segment between barriers
         for i in range(1, len(barriers)):
             # The segment starts one section after the previous barrier
             # and ends one section before the current barrier
-            seg_start = barriers[i-1] + 1
+            seg_start = barriers[i - 1] + 1
             seg_end = barriers[i]
 
-            '''Create a new array of segments and calculates the sum directly'''
+            """Create a new array of segments and calculates the sum directly"""
             segment = scenic_values[seg_start:seg_end]
             current_trip_value = sum(segment)
 
@@ -48,7 +46,7 @@ def solve():
 
             if current_trip_value > max_trip_value:
                 max_trip_value = current_trip_value
-        
+
         if driveable:
             print(max_trip_value)
         else:
@@ -56,6 +54,7 @@ def solve():
 
     # end_time = time.perf_counter()
     # print(f"Time: {end_time - start_time:.6f} seconds")
+
 
 if __name__ == "__main__":
     solve()
